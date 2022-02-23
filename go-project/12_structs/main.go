66555cbef4f688a83d44fa84386f923644b4c87c
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-02-24 00:53:14 +0330

    saw session 12

---
+++ go-project/12_structs/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// OOP
+
+type Trade struct {
+	// if property starts with captal letter that's poblic otherwise is private
+	Symbol string  // stock symbol
+	Value  int     // Number of shares
+	Price  float64 // Trade Price
+	Buy    bool    // true if buy trade. false if sell trade
+}
+
+func main() {
+	t1 := Trade{"APPLE", 10, 99.98, true}
+	fmt.Println(t1)
+	fmt.Printf("%+v\n", t1)
+	fmt.Printf("Price: %+v\n", t1.Price)
+
+	fmt.Println("=======================================")
+
+	t2 := Trade{
+		Value:  15,
+		Symbol: "MICROSOFT",
+		Price:  99.97,
+		Buy:    false, // trailing come is nandatory
+	}
+
+	fmt.Println(t2)
+	fmt.Printf("%+v\n", t2)
+
+	fmt.Println("=======================================")
+
+	t3 := Trade{}
+	fmt.Println(t3)
+	fmt.Printf("%+v\n", t3)
+}
